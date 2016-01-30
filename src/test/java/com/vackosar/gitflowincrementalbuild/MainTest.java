39256f3ab79687e5c70308616be4579832885a55
vakosar <vackosar>
2016-01-30 11:41:43 +0100

    Implement Main.

--- src/test/java/com/vackosar/gitflowincrementalbuild/MainTest.java
+++ src/test/java/com/vackosar/gitflowincrementalbuild/MainTest.java
@@ -1,20 +1,31 @@
 package com.vackosar.gitflowincrementalbuild;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.File;
 
 public class MainTest {
+
     @Test
     public void list() throws Exception {
         RepoMock repoMock = new RepoMock();
-        final Path pom = Paths.get(RepoMock.WORK_DIR + "parent/pom.xml");
         String workDir = DiffListerTest.TEST_WORK_DIR + "tmp/repo/";
         System.setProperty("user.dir", workDir);
-        Path[] expected = new Path[] {Paths.get("child1")};
-        Main.main(new String[]{pom.toString()});
-//        Assert.assertArrayEquals("child1", );
+        final Process process =
+                new ProcessBuilder("java", "-cp", "../../target/*", "com.vackosar.gitflowincrementalbuild.Main", "parent/pom.xml")
+                        .directory(new File("tmp/repo"))
+                        .start();
+        while (process.isAlive()) {
+            Thread.sleep(20);
+        }
+        Assert.assertEquals("child1" + System.lineSeparator(), convertStreamToString(process.getInputStream()));
+        Assert.assertEquals("", convertStreamToString(process.getErrorStream()));
         repoMock.close();
+    }
+
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 }
