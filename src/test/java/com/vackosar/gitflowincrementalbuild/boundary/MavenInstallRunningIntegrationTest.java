b8a7da5ab24aeefa764f196e88f5b06955add979
Falko Modler <famod@users.noreply.github.com>
2018-06-23 18:26:07 +0200

    Fix & improve MavenInstallRunningIntegrationTest
    - adjust to be runnable on Windows without manual editing
    - verify return code of each mvn invocation
    - "-skipTest" in installCurrentVersion() is not a valid argument
      "-Dmaven.test.skip=true" seems more appropriate
    - "clean" in installCurrentVersion() fails (on Windows) because
      the surefirebooter jar is being used a the same time
    - install (and remove) test artifacts to fix buildWithoutAlsoMake()
      which fails because "testJarDependent" needs "testJarDependency"
      in local repo
    - some other small improvements

--- src/test/java/com/vackosar/gitflowincrementalbuild/boundary/MavenInstallRunningIntegrationTest.java
+++ src/test/java/com/vackosar/gitflowincrementalbuild/boundary/MavenInstallRunningIntegrationTest.java
@@ -2,9 +2,13 @@ package com.vackosar.gitflowincrementalbuild.boundary;
 
 import com.vackosar.gitflowincrementalbuild.BaseRepoTest;
 import com.vackosar.gitflowincrementalbuild.control.Property;
+
+import org.apache.commons.lang3.SystemUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.ResetCommand;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -22,11 +26,49 @@ import java.util.stream.Stream;
  */
 public class MavenInstallRunningIntegrationTest extends BaseRepoTest {
 
+    private static final String DEFAULT_POMFILE_ARG = "--file=parent/pom.xml";
+
+    private static String gibVersionArg;
+
     @BeforeClass
-    public static void installCurrentVersion() throws IOException {
-        new ProcessBuilder("mvn", "clean", "install", "-skipTest").start();
+    public static void installCurrentVersion() throws IOException, InterruptedException {
+        awaitProcess(new ProcessBuilder(cmdArgs("mvn", "install", "-Dmaven.test.skip=true")).start());
+        // Get current version from pom.xml
+        String version = Files.readAllLines(Paths.get("pom.xml")).stream().filter(s -> s.contains("<version>")).findFirst().get()
+            .replaceAll("</*version>", "").replaceAll("^[ \t]*", "");
+        gibVersionArg = "-DgibVersion=" + version;
     }
 
+    /**
+     * Installs all test artifacts/modules to avoid dependency problems when only building a subset incrementally. 
+     *
+     * @throws IOException on process execution errors
+     * @throws InterruptedException on process execution errors
+     */
+    @Before
+    public void installTestArtifacts() throws IOException, InterruptedException {
+        awaitProcess(
+            new ProcessBuilder(cmdArgs("mvn", "install",
+                DEFAULT_POMFILE_ARG, gibVersionArg, "-Dgib." + Property.enabled + "=false"))
+                    .directory(getLocalRepoMock().getBaseCanonicalBaseFolder())
+                    .start());
+    }
+
+    /**
+     * Removes the test artifacts/modules which have been installed by {@link #installTestArtifacts()}.
+     *
+     * @throws IOException on process execution errors
+     * @throws InterruptedException on process execution errors
+     */
+    @After
+    public void purgeInstalledTestArtifacts() throws IOException, InterruptedException {
+        awaitProcess(
+            new ProcessBuilder(cmdArgs("mvn", "org.codehaus.mojo:build-helper-maven-plugin:3.0.0:remove-project-artifact",
+                DEFAULT_POMFILE_ARG, gibVersionArg,"-Dgib." + Property.enabled + "=false"))
+                    .directory(getLocalRepoMock().getBaseCanonicalBaseFolder())
+                    .start());
+    }
+    
     @Test
     public void worktreeFails() throws Exception {
         final String output = executeBuild(Collections.singletonList("--file=wrkf2/parent/pom.xml"));
@@ -136,15 +178,10 @@ public class MavenInstallRunningIntegrationTest extends BaseRepoTest {
     }
 
     private String executeBuild(List<String> args) throws IOException, InterruptedException {
-        String version = Files.readAllLines(Paths.get("pom.xml")).stream().filter(s -> s.contains("<version>")).findFirst().get().replaceAll("</*version>", "").replaceAll("^[ \t]*", "");
-        final List<String> commandBase = Arrays.asList(
-//                "cmd", "/c" // Uncomment for Windows.
-                "mvn",
-                "package",
-                "-DgibVersion=" + version);
+        final List<String> commandBase = cmdArgs("mvn", "package", gibVersionArg);
         final List<String> commandBaseWithFile;
         if (args.stream().noneMatch(s->s.startsWith("--file"))) {
-            commandBaseWithFile = Stream.concat(commandBase.stream(), Stream.of("--file=parent/pom.xml")).collect(Collectors.toList());
+            commandBaseWithFile = Stream.concat(commandBase.stream(), Stream.of(DEFAULT_POMFILE_ARG)).collect(Collectors.toList());
         } else {
             commandBaseWithFile = commandBase;
         }
@@ -153,14 +190,29 @@ public class MavenInstallRunningIntegrationTest extends BaseRepoTest {
                 new ProcessBuilder(command)
                         .directory(getLocalRepoMock().getBaseCanonicalBaseFolder())
                         .start();
-        String output = convertStreamToString(process.getInputStream());
-        System.out.println(convertStreamToString(process.getErrorStream()));
-        process.waitFor();
-        return output;
+        return awaitProcess(process);
+    }
+
+    private static String awaitProcess(Process process) throws InterruptedException {
+        final String stdOut = convertStreamToString(process.getInputStream());
+        final String stdErr = convertStreamToString(process.getErrorStream());
+        final int returnCode = process.waitFor();
+        if (returnCode > 0) {
+            System.err.println(stdOut);
+            System.err.println(stdErr);
+            Assert.fail("Process failed with return code " + returnCode);
+        }
+        return stdOut;
     }
 
     private static String convertStreamToString(java.io.InputStream is) {
         java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
         return s.hasNext() ? s.next() : "";
+    }
+
+    private static List<String> cmdArgs(String... args) {
+        return SystemUtils.IS_OS_WINDOWS
+            ? Stream.concat(Stream.of("cmd", "/c"), Arrays.stream(args)).collect(Collectors.toList())
+            : Arrays.asList(args);
     }
 }
