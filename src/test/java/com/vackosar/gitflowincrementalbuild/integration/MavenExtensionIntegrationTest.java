0da4df15dfb41b7816fb23b16293167c8e2180e1
Falko Modler <famod@users.noreply.github.com>
2024-09-16 22:50:28 +0200

    [3.x] Provide plexus-utils 1.1 to integration tests
    ...without breaking Xpp3Dom casting when using GIB as a core extension.
    
    This is basically a combined cherry-pick of 3bf43237d0edef6f835300d5bf706b55a2c572ad
    and bf2481333c03ab025703e45f6e85bac539880790 (and also ba473913122cdcbefcc9d8f7d7da7cdbf44f713e).

--- src/test/java/com/vackosar/gitflowincrementalbuild/integration/MavenExtensionIntegrationTest.java
+++ src/test/java/com/vackosar/gitflowincrementalbuild/integration/MavenExtensionIntegrationTest.java
@@ -2,7 +2,7 @@ package com.vackosar.gitflowincrementalbuild.integration;
 
 /**
  * Integration test running the {@code mvn} command on a test project with active {@code gitflow-incremental-builder}
- * registered as a Maven plugin (not directly as an extension).
+ * registered directly as a Maven extension (not as a plugin) in {@code pom.xml}.
  */
 public class MavenExtensionIntegrationTest extends MavenIntegrationTestBase {
 
